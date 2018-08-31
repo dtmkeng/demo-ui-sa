f906ecf4c039c70c1bcf81346a3f720a8b003e26
dtmkeng <keng1998@outlook.com>
2018-08-31 14:40:19 +0700

    add check form and detail form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule,MatIconModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CardListResverComponent } from './card-list-resver/card-list-resver.component';
+import { CheckFormComponent } from './check-form/check-form.component';
+import { DetailFormComponent } from './detail-form/detail-form.component';
 
 // import {CardListResverComponent} from './card-list-resver/card-list-resver.component' 
 @NgModule({
   declarations: [
     AppComponent,
-    CardListResverComponent
+    CardListResverComponent,
+    CheckFormComponent,
+    DetailFormComponent
   ],
   imports: [
     BrowserModule,
